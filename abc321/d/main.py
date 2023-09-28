a7807e132776ac0443024a0e0149403a1bdf3e34
radiol <radiol@github.com>
2023-09-28 15:42:13 +0900

    ABC321 D: AC

--- abc321/d/main.py
+++ abc321/d/main.py
@@ -1,31 +1,25 @@
-import sys
-
-sys.setrecursionlimit(10**6)
-
-input = sys.stdin.readline
-
-
-def debug(*args, sep=None):
-    if sep is None and hasattr(args[0], "__iter__"):
-        sep = "\n"
-    print("Debug:", *args, file=sys.stderr, sep=sep)
+from bisect import bisect_right
 
 
 def main():
-    N = int(input())
-    N, M = map(int, input().split())
+    N, M, P = map(int, input().split())
     A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
 
-    # M行dataの読み込み
-    for _ in range(M):
-        u, v = map(int, input().split())
+    B.sort()
 
-    H, W = map(int, input().split())
-    # x行y列のデータ(x:0~H-1, y:0~W-1)の取得はgrid[x][y]
-    # '.'や'#'で表現される文字列のデータの場合
-    grid = [list(input()) for _ in range(H)]
-    # 数値データの場合
-    grid = [list(map(int, input().split())) for _ in range(H)]
+    cum_B = [0] * (len(B) + 1)
+
+    for i, b in enumerate(B):
+        cum_B[i + 1] = cum_B[i] + b
+
+    ans = 0
+
+    for a in A:
+        index = bisect_right(B, P - a)
+        ans += cum_B[index] + a * index + P * (M - index)
+
+    print(ans)
 
 
 if __name__ == "__main__":
