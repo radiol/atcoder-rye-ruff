4e57210fb6ce12ad1e236f18b35df3f5595a38dc
radiol <radiol@github.com>
2023-10-09 23:49:25 +0900

    ABC322A-C: AC

--- abc322/b/main.py
+++ abc322/b/main.py
@@ -1,31 +1,17 @@
-import sys
-
-sys.setrecursionlimit(10**6)
-
-input = sys.stdin.readline
-
-
-def debug(*args, sep=None):
-    if sep is None and hasattr(args[0], "__iter__"):
-        sep = "\n"
-    print("Debug:", *args, file=sys.stderr, sep=sep)
-
-
 def main():
-    N = int(input())
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
+    N, _ = map(int, input().split())
+    S = input()
+    T = input()
 
-    # M行dataの読み込み
-    for _ in range(M):
-        u, v = map(int, input().split())
+    ans = 0
+    is_top_match = T[:N] == S
+    is_tail_match = T[-N:] == S
 
-    H, W = map(int, input().split())
-    # x行y列のデータ(x:0~H-1, y:0~W-1)の取得はgrid[x][y]
-    # '.'や'#'で表現される文字列のデータの場合
-    grid = [list(input()) for _ in range(H)]
-    # 数値データの場合
-    grid = [list(map(int, input().split())) for _ in range(H)]
+    if not is_tail_match:
+        ans += 1
+    if not is_top_match:
+        ans += 2
+    print(ans)
 
 
 if __name__ == "__main__":
--- abc322/c/main.py
+++ abc322/c/main.py
@@ -5,27 +5,20 @@ sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 
-def debug(*args, sep=None):
-    if sep is None and hasattr(args[0], "__iter__"):
-        sep = "\n"
-    print("Debug:", *args, file=sys.stderr, sep=sep)
-
-
 def main():
-    N = int(input())
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
+    N, _ = map(int, input().split())
+    A = [0, *list(map(int, input().split()))]
 
-    # M行dataの読み込み
-    for _ in range(M):
-        u, v = map(int, input().split())
+    ans = [0] * (N + 1)
 
-    H, W = map(int, input().split())
-    # x行y列のデータ(x:0~H-1, y:0~W-1)の取得はgrid[x][y]
-    # '.'や'#'で表現される文字列のデータの場合
-    grid = [list(input()) for _ in range(H)]
-    # 数値データの場合
-    grid = [list(map(int, input().split())) for _ in range(H)]
+    nxt_fireworks = A.pop()
+    for i in range(N, 0, -1):
+        if i == nxt_fireworks:
+            ans[i] = 0
+            nxt_fireworks = A.pop()
+        else:
+            ans[i] = ans[i + 1] + 1
+    print(*ans[1:], sep="\n")
 
 
 if __name__ == "__main__":
