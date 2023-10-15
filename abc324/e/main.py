06bccdabf98d5a3376313a79ae463d8d9b2d09a0
radiol <radiol@github.com>
2023-10-15 16:11:53 +0900

    ABC324E: Edit

--- abc324/e/main.py
+++ abc324/e/main.py
@@ -12,20 +12,43 @@ def debug(*args, sep=None):
 
 
 def main():
-    N = int(input())
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
+    N, T = input().split()
+    N = int(N)
+    T = T.strip()
 
-    # M行dataの読み込み
-    for _ in range(M):
-        u, v = map(int, input().split())
+    forward_match_cnt = [0] * (len(T) + 1)
+    backward_match_cnt = [0] * (len(T) + 1)
 
-    H, W = map(int, input().split())
-    # x行y列のデータ(x:0~H-1, y:0~W-1)の取得はgrid[x][y]
-    # '.'や'#'で表現される文字列のデータの場合
-    grid = [list(input()) for _ in range(H)]
-    # 数値データの場合
-    grid = [list(map(int, input().split())) for _ in range(H)]
+    forward_match_cnt[0] = backward_match_cnt[0] = N
+
+    for _ in range(N):
+        S = input().strip()
+        idx = 0
+        for s in S:
+            if s == T[idx]:
+                idx += 1
+                forward_match_cnt[idx] += 1
+            if idx >= len(T):
+                break
+
+        idx = 1
+        for s in reversed(S):
+            if s == T[len(T) - idx]:
+                backward_match_cnt[idx] += 1
+                idx += 1
+            if idx > len(T):
+                break
+        # debug(forward_match_cnt)
+        # debug(backward_match_cnt)
+
+        last_backward = 0
+        ans = 0
+        for f, b in zip(forward_match_cnt, reversed(backward_match_cnt)):
+            cnt = f * (b - last_backward)
+            if cnt != 0:
+                ans += cnt
+                last_backward = b
+    print(ans)
 
 
 if __name__ == "__main__":
