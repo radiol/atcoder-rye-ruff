3f349eeb46e76b44278cb2c444a5a3777b1dd1c0
radiol <radiol@github.com>
2023-10-21 23:34:19 +0900

    ABC323E: AC

--- abc323/e/main.py
+++ abc323/e/main.py
@@ -11,21 +11,32 @@ def debug(*args, sep=None):
     print("Debug:", *args, file=sys.stderr, sep=sep)
 
 
+MOD = 998244353
+
+
 def main():
-    N = int(input())
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
+    N, X = map(int, input().split())
+    T = list(map(int, input().split()))
+    # 全体の開始からx秒後に曲nがちょうど終了する確率dp[n][x] ignore
+    # X秒までで曲が終了し、X+1秒の時点でどの曲がかかっているかが答えなので0~X+1まで
+    dp = [[0] * (X + 2) for _ in range(N)]
 
-    # M行dataの読み込み
-    for _ in range(M):
-        u, v = map(int, input().split())
+    # 1/Nの逆元invを求める
+    inv = pow(N, -1, MOD)
 
-    H, W = map(int, input().split())
-    # x行y列のデータ(x:0~H-1, y:0~W-1)の取得はgrid[x][y]
-    # '.'や'#'で表現される文字列のデータの場合
-    grid = [list(input()) for _ in range(H)]
-    # 数値データの場合
-    grid = [list(map(int, input().split())) for _ in range(H)]
+    dp[0][0] += 1
+
+    for x in range(X + 1):
+        # x秒後にちょうど曲が終わる確率(全ての曲の合計) ignore
+        temp_sum = sum(dp[n][x] for n in range(N)) % MOD
+        if temp_sum == 0:
+            continue
+        for n in range(N):
+            # 次の曲が終わるのがx+T[n]秒後、x+T[n]>=X+1ならX+1秒後に曲nがかかっている
+            next_end = min(X + 1, x + T[n])
+            dp[n][next_end] += temp_sum * inv
+            dp[n][next_end] %= MOD
+    print(dp[0][-1])
 
 
 if __name__ == "__main__":
