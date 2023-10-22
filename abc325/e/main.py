6591ef8cf9439833c586fe491555a2c6be8ee014
radiol <radiol@github.com>
2023-10-22 18:46:34 +0900

    ABC325E:AC

--- abc325/e/main.py
+++ abc325/e/main.py
@@ -1,4 +1,5 @@
 import sys
+from heapq import heappop, heappush
 
 sys.setrecursionlimit(10**6)
 
@@ -12,20 +13,45 @@ def debug(*args, sep=None):
 
 
 def main():
-    N = int(input())
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
+    N, A, B, C = map(int, input().split())
 
-    # M行dataの読み込み
-    for _ in range(M):
-        u, v = map(int, input().split())
+    D = [list(map(int, input().split())) for _ in range(N)]
 
-    H, W = map(int, input().split())
-    # x行y列のデータ(x:0~H-1, y:0~W-1)の取得はgrid[x][y]
-    # '.'や'#'で表現される文字列のデータの場合
-    grid = [list(input()) for _ in range(H)]
-    # 数値データの場合
-    grid = [list(map(int, input().split())) for _ in range(H)]
+    hq = []
+    heappush(hq, (0, 0))
+    marked = [False] * N
+
+    car_times = [float("inf")] * N
+    car_times[0] = 0
+
+    while hq:
+        t, crr = heappop(hq)
+        marked[crr] = True
+        for nxt in range(N):
+            if marked[nxt]:
+                continue
+            nxt_t = t + D[crr][nxt] * A
+            if nxt_t < car_times[nxt]:
+                car_times[nxt] = nxt_t
+                heappush(hq, (nxt_t, nxt))
+
+    hq = []
+    heappush(hq, (0, N - 1))
+    marked = [False] * N
+    train_times = [float("inf")] * N
+    train_times[N - 1] = 0
+    while hq:
+        t, crr = heappop(hq)
+        marked[crr] = True
+        for nxt in range(N):
+            if marked[nxt]:
+                continue
+            nxt_t = t + D[crr][nxt] * B + C
+            if nxt_t < train_times[nxt]:
+                train_times[nxt] = nxt_t
+                heappush(hq, (nxt_t, nxt))
+
+    print(min([c + t for c, t in zip(car_times, train_times)]))
 
 
 if __name__ == "__main__":
