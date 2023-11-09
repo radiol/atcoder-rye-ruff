f34d67c20b572c5246461ff6ac7f80d8b1303a0a
radiol <radiol@github.com>
2023-11-10 01:37:33 +0900

    ABC313D: Add comment

--- abc313/d/main.py
+++ abc313/d/main.py
@@ -16,25 +16,26 @@ def debug(*args, sep=None):
 def main():
     N, K = map(int, input().split())
     X = list(map(str, range(1, N + 1)))
-    ans = [0] * (N + 1)
-    q_cnt = 0
+    ans = [0] * N
+    # x1, x2, ..., xk-1のk-1個とxk~xNのうちから1つの計k個の偶奇を求める
+    # これをN-K+1回繰り返す
     for i in range(K - 1, N):
-        ans[i + 1] = ask(X[: K - 1] + [X[i]])
-        q_cnt += 1
+        ans[i] = ask(X[: K - 1] + [X[i]])
+    # x1, x2, .... xi-1, xi+1, ...xk+1のk個の偶奇を求める
+    # これをK-1回繰り返す
     for i in range(K - 1):
         res = ask(X[:i] + X[i + 1 : K + 1])
-        q_cnt += 1
-        if res == ans[K + 1]:
-            ans[i + 1] = ans[K]
+        # ans[K]はx1, x2, ..., xk-1, xk+1の偶奇
+        # ans[K]と偶奇が一致するなら、xiの偶奇はxkと同じ
+        if res == ans[K]:
+            ans[i] = ans[K - 1]
         else:
-            ans[i + 1] = 1 - ans[K]
-    debug(ans)
-    debug(q_cnt)
-
-    if sum(ans[1 : K + 1]) % 2 != ans[K]:
+            ans[i] = 1 - ans[K - 1]
+    # x1, x2, ... xkの奇数の数がans[K-1]と一致しないなら偶奇を反転させる
+    if sum(ans[:K]) % 2 != ans[K - 1]:
         ans = [1 - x for x in ans]
 
-    print(f"! {' '.join(map(str, ans[1:]))}", flush=True)
+    print(f"! {' '.join(map(str, ans))}", flush=True)
 
 
 def ask(x: list[str]) -> int:
