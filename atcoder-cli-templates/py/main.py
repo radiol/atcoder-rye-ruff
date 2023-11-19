6a9d7ddb1c539719be813c7ad37e72b470adb1f7
radiol <radiol@github.com>
2023-11-19 17:57:08 +0900

    Edit main.py template.

--- atcoder-cli-templates/py/main.py
+++ atcoder-cli-templates/py/main.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import sys
 
 sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
+MOD = 998244353
 
 
 def debug(*args, sep=None):
@@ -13,19 +16,19 @@ def debug(*args, sep=None):
 
 def main():
     N = int(input())
-    N, M = map(int, input().split())
-    A = list(map(int, input().split()))
+    N, M = (int(x) for x in input().split())
+    A = [int(x) for x in input().split()]
 
     # M行dataの読み込み
     for _ in range(M):
-        u, v = map(int, input().split())
+        u, v = (int(x) for x in input().split())
 
-    H, W = map(int, input().split())
+    H, W = (int(x) for x in input().split())
     # x行y列のデータ(x:0~H-1, y:0~W-1)の取得はgrid[x][y]
     # '.'や'#'で表現される文字列のデータの場合
-    grid = [list(input()) for _ in range(H)]
+    grid = [list(input().strip()) for _ in range(H)]
     # 数値データの場合
-    grid = [list(map(int, input().split())) for _ in range(H)]
+    grid = [[int(x) for x in input().split()] for _ in range(H)]
 
 
 if __name__ == "__main__":
