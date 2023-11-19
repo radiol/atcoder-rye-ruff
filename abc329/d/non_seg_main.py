a20e22cf6cc54be8d0694d77f26fb0ead9af6e4c
radiol <radiol@github.com>
2023-11-19 17:36:24 +0900

    ABC329D: Solved without segment tree

---
+++ abc329/d/non_seg_main.py
@@ -0,0 +1,16 @@
+def main():
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    cnt = [0] * (N + 1)
+
+    top = 0
+    for a in A:
+        cnt[a] += 1
+        if cnt[a] > cnt[top] or (cnt[a] == cnt[top] and a < top):
+            top = a
+        print(top)
+
+
+if __name__ == "__main__":
+    main()
