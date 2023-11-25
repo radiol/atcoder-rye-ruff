edc8cfca10e99339def5b30000e5cee2575943cf
radiol <radiol@github.com>
2023-11-25 13:24:00 +0900

    ABC307C: AC

--- abc307/c/main.py
+++ abc307/c/main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import sys
+from itertools import product
 
 sys.setrecursionlimit(10**6)
 
@@ -15,20 +16,53 @@ def debug(*args, sep=None):
 
 
 def main():
-    N = int(input())
-    N, M = (int(x) for x in input().split())
-    A = [int(x) for x in input().split()]
+    HA, WA = (int(x) for x in input().split())
+    A = [list(input().rstrip()) for _ in range(HA)]
+    HB, WB = (int(x) for x in input().split())
+    B = [list(input().rstrip()) for _ in range(HB)]
+    HX, WX = (int(x) for x in input().split())
+    X = [list(input().rstrip()) for _ in range(HX)]
 
-    # M行dataの読み込み
-    for _ in range(M):
-        u, v = (int(x) for x in input().split())
+    total_cnt = sum(row.count("#") for row in A) + sum(row.count("#") for row in B)
 
-    H, W = (int(x) for x in input().split())
-    # x行y列のデータ(x:0~H-1, y:0~W-1)の取得はgrid[x][y]
-    # '.'や'#'で表現される文字列のデータの場合
-    grid = [list(input().strip()) for _ in range(H)]
-    # 数値データの場合
-    grid = [[int(x) for x in input().split()] for _ in range(H)]
+    def add(C, sheet, x, y):
+        for i, row in enumerate(sheet):
+            for j, c in enumerate(row):
+                if c == "#":
+                    C[y + i][x + j] += 1
+        return C
+
+    def check(C):
+        cnt = 0
+        for i, row in enumerate(X, start=10):
+            for j, c in enumerate(row, start=10):
+                if c == "#" and C[i][j] == 0:
+                    break
+                if c == "." and C[i][j] >= 1:
+                    break
+                cnt += C[i][j]
+            else:
+                continue
+            break
+        else:
+            return cnt == total_cnt
+        return False
+
+    HC = 30
+    WC = 30
+    for xa, ya, xb, yb in product(
+        range(20),
+        range(20),
+        range(20),
+        range(20),
+    ):
+        C = [[0] * WC for _ in range(HC)]
+        C = add(C, A, xa, ya)
+        C = add(C, B, xb, yb)
+        if check(C):
+            print("Yes")
+            return
+    print("No")
 
 
 if __name__ == "__main__":
